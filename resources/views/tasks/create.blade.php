3f7bcc9af250443ccbd7121189a5e07ebc624509
EC2 Default User <ec2-user@ip-172-31-71-103.ec2.internal>
2018-06-07 17:33:59 +0900

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,17 +2,25 @@
 
 @section('content')
 
-<h1>メッセージ新規作成ページ</h1>
+<h1>タスク新規作成ページ</h1>
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::select('status', ['' => '選択してください','完了' => '完了', '進行中' => '進行中', '未完了' => '未完了']) !!}
+    <div class="row">
+        <div class="col-xs-12 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::select('status', ['' => '選択してください','完了' => '完了', '進行中' => '進行中', '未完了' => '未完了'], null, ['class' => 'form-control']) !!}
+            </div>
 
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+            <div class="form-group">
+                {!! Form::label('content', 'タスク:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+        </div>
+    </div>
 
-        {!! Form::submit('投稿') !!}
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
 
